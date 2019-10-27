015c3cb01218b9e3fbe4a10d66d52960678106a1
Jon Finley <lilfin19@gmail.com>
2019-10-27 10:32:26 -0500

    got links working still working on readme and github links

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserve friends.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Jon Finley',
+    profilePic: 'https://avatars3.githubusercontent.com/u/17751317?v=3&s=466',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
+  
